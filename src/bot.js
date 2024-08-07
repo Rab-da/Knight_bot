8921831bc6894502b5d45e9d32a346aa2e1a1a40
Raon <rabda6022@gmail.com>
2024-08-07 15:14:20 +0900

    공지이벤트임베드

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const { Token } = process.env;
-const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
+const { channel } = require('diagnostics_channel');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000; // 포트를 환경 변수에서 가져오거나 기본값으로 3000을 사용
 
+const client = new Client({
+    intents: [
+        32767,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
 
-const client = new Client({ intents: 32767 });
 client.commands = new Collection();
 client.commandArray = [];
 client.buttons = new Collection();
@@ -25,16 +30,37 @@ for (const folder of functionFolders) {
     require(`./functions/${folder}/${file}`)(client);
 };
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+client.on('messageCreate', async (message) => {
+    const targetChannelIds = [
+        '1268751450763427971',
+        '1268530035816529940',
+        '1270624005149425685',
+    ];
+    // 봇 자신의 메시지나 시스템 메시지는 무시
+    if (message.author.bot) return;
+
+    // 특정 채널에서만 반응하게 하려면 조건 추가 (예: 채널 ID)
+    if (!targetChannelIds.includes(message.channel.id)) return;
+
+
+    // 메시지를 임베드로 변환
+    const embed = new EmbedBuilder()
+        .setDescription(message.content)
+        .setTimestamp()
+        .setColor('#00a19c')
+        .setAuthor({ name: message.author.username, iconURL: message.author.displayAvatarURL() })
+        .setFooter({ text: `Message by: ${message.author.tag}` });
+
+    // 임베드 메시지를 보낸 후 원본 메시지를 삭제 (원하지 않으면 삭제 부분 제거)
+    await message.channel.send({ embeds: [embed] });
+    await message.delete();
+});
 
 
 client.handleEvents();
 client.handleCommands();
 client.handleComponents();
 client.login(Token);
-client
 // (async () => {
 //     connect(databaseToken).catch(console.error);
 // })();
